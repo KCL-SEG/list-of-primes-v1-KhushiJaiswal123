fc3ba67e7e9df4981d181d0903279619ca5a30f9
Khushi Jaiswal <14khushijais@gmail.com>
2022-10-12 19:48:14 +0100

    complete print prime numbers exercise

--- primes.py
+++ primes.py
@@ -1,6 +1,16 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
 
 def primes(number_of_primes):
     list = []
+    numerator = 1
+    while len(list) < number_of_primes:
+        numerator = numerator+1
+        for denominator in range (2,numerator):
+            if (numerator%denominator == 0):
+                break
+        else:
+            list.append(numerator)
+
     return list
